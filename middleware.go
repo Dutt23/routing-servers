a3a0db890bd0aebd2e6164e197b76817aad45e2a
Shatyaki Dutt <shatyaki_dutt@hotmail.com>
2021-03-07 20:43:28 +0530

    routing

--- middleware.go
+++ middleware.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 type logger struct {
@@ -20,8 +22,24 @@ func hello(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprint(w, "Hello\n")
 }
 
+func greet(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprint(w, "hi foo")
+}
+func greetUser(w http.ResponseWriter, req *http.Request) {
+	user := mux.Vars(req)["user"]
+	fmt.Fprintf(w, "hi %s\n", user)
+}
+func defineRouters(router *mux.Router) {
+	router.HandleFunc("/foo", greet).Methods("GET")
+	router.HandleFunc("/host", greet).Methods("GET").Host("www.foo.com")
+	// Url will take only characters
+	router.HandleFunc("/users/{user:[a-z,A-Z]+}", greetUser).Methods("GET")
+}
+
 func main() {
-	h := http.HandlerFunc(hello)
-	logger := logger{hanlder: h}
+	// h := http.HandlerFunc(hello)
+	r := mux.NewRouter()
+	defineRouters(r)
+	logger := logger{hanlder: r}
 	http.ListenAndServe(":8000", &logger)
 }
